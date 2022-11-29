ec831170a292826971cc33092dbedd2e8ed22136
vinicius cardoso <cardosovinicius87@gmail.com>
2022-11-29 19:02:35 -0300

    feat: model Products

---
+++ back-end/src/database/models/Products.js
@@ -0,0 +1,15 @@
+module.exports = (sequelize, DataTypes) => {
+  const Product = sequelize.define('Product', {
+    id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
+    name: DataTypes.STRING,
+    price: DataTypes.DECIMAL,
+    url_image: DataTypes.STRING,
+  },
+  {
+    timestamps: false,
+    tableName: 'products',
+    underscored: true,
+  });
+
+  return Product;
+};
