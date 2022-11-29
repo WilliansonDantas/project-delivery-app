355652c4fdacd76f2a09b088c3afa4043d402057
vinicius cardoso <cardosovinicius87@gmail.com>
2022-11-29 16:03:21 -0300

    feat: migration sales

---
+++ back-end/src/database/migrations/20221129185525-sales.js
@@ -0,0 +1,44 @@
+'use strict';
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    return queryInterface.createTable('sales', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      user_id: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        onUpdate: 'CASCADE',
+        onDelete: 'CASCADE',
+        references: {
+          model: 'users',
+          key: 'id',
+        },
+      },
+      seller_id: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        onUpdate: 'CASCADE',
+        onDelete: 'CASCADE',
+        references: {
+          model: 'users',
+          key: 'id',
+        },
+      },
+      delivery_address: {
+        allowNull: false,
+        type: Sequelize.STRING,
+      },
+      sale_date: {
+        type: Sequelize.DATE,
+      },
+    })
+  },
+  down: async (queryInterface, Sequelize) => {
+    return queryInterface.dropTable('sales');
+  }
+};
