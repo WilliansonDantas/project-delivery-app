21ca3418bd2ecd094edf8038e369aed7721aebe6
vinicius cardoso <cardosovinicius87@gmail.com>
2022-11-29 16:02:49 -0300

    feat: migration users

---
+++ back-end/src/database/migrations/20221129183922-users.js
@@ -0,0 +1,37 @@
+'use strict';
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    return queryInterface.createTable('users', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      name: {
+        allowNull: false,
+        type: Sequelize.STRING,
+      },
+      email: {
+        allowNull: false,
+        type: Sequelize.STRING,
+        unique: true
+      },
+      password: {
+        allowNull: false,
+        type: Sequelize.STRING,
+      },
+      role: {
+        allowNull: false,
+        type: Sequelize.STRING,
+      }
+    })
+  },
+
+  // definiçao do tamanho dos campos
+  
+  down: async (queryInterface, Sequelize) => {
+    return queryInterface.dropTable('users');
+  }
+};
