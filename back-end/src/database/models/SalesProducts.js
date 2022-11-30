58486c6eaadf06322d6297b8a3262a59b22b6a1f
Igor Brizack <igor_brizack@hotmail.com>
2022-11-30 11:05:52 -0300

    feat: created seeders, model and migrations

--- back-end/src/database/models/SalesProducts.js
+++ back-end/src/database/models/SalesProducts.js
@@ -13,14 +13,14 @@ module.exports = (sequelize, DataTypes) => {
   SalesProducts.associate = (models) => {
     models.Product.belongsToMany(models.Sale, {
       as: 'sales',
-      through: UserBook,
-      foreignKey: 'productId', // se refere ao id de Book na tabela de `users_books`
-      otherKey: 'saleId', // se refere a outra chave de `users_books` 
+      through: SalesProducts,
+      foreignKey: 'productId', 
+      otherKey: 'saleId', 
     });
     models.Sale.belongsToMany(models.Product, {
       as: 'products',
-      through: UserBook,
-      foreignKey: 'saleId',  // se refere ao id de User na tabela de `users_books`
+      through: SalesProducts,
+      foreignKey: 'saleId',  
       otherKey: 'productId',
     });
   };
