8f681785d161b35721ffda67ae969552918a2342
vinicius cardoso <cardosovinicius87@gmail.com>
2022-11-30 18:28:15 -0300

    fix: switch

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,11 +1,11 @@
-import { Routes, Route } from 'react-router-dom';
+import { Route, Switch } from 'react-router-dom';
 import React from 'react';
 import Login from './pages/Login';
 import LoginProvider from './contexts/LoginProvider';
 
 function App() {
   return (
-    <Routes>
+    <Switch>
       <Route
         path="/login"
         element={
@@ -23,7 +23,7 @@ function App() {
           </LoginProvider>
         }
       />
-    </Routes>
+    </Switch>
   );
 }
 
