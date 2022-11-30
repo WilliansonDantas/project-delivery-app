39f3d55323d5af4a38f29b09fc0a3540153d1afc
vinicius cardoso <cardosovinicius87@gmail.com>
2022-11-30 17:51:54 -0300

    fix: erros de lint

--- back-end/src/routes/login.js
+++ back-end/src/routes/login.js
@@ -1,9 +1,9 @@
-const { Router } = require("express");
+const { Router } = require('express');
 
-const loginRouter = Router()
+const loginRouter = Router();
 
-const { getToken } = require('../controller/loginCrontoller')
+const { getToken } = require('../controller/loginCrontoller');
 
-loginRouter.post('/login', (req, res, next) => getToken(req,res,next));
+loginRouter.post('/login', (req, res, next) => getToken(req, res, next));
 
 module.exports = loginRouter;
