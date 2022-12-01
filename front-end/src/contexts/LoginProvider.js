ae9464e79a4376a69f75aa352156b3d9e8eda02a
vinicius cardoso <cardosovinicius87@gmail.com>
2022-12-01 17:48:50 -0300

    feat: validacao usuario

--- front-end/src/contexts/LoginProvider.js
+++ front-end/src/contexts/LoginProvider.js
@@ -1,17 +1,28 @@
-import React, { useMemo, useState } from 'react';
+import React, { useEffect, useMemo, useState } from 'react';
 import PropTypes from 'prop-types';
 import LoginContext from './LoginContext';
 
 function LoginProvider({ children }) {
   const [email, setEmail] = useState();
-  const [password, setPassword] = useState();
+  const [password, setPassword] = useState('');
+  const [valid, setValid] = useState(false);
+
+  useEffect(() => {
+    const senhaLength = 5;
+    if (/\S+@\S+\.\S+/.test(email) && password.length > senhaLength) {
+      setValid(true);
+    } else {
+      setValid(false);
+    }
+  }, [email, password]);
 
   const contextUser = useMemo(() => ({
     email,
     setEmail,
     password,
     setPassword,
-  }), [email, password]);
+    valid,
+  }), [email, password, valid]);
 
   return (
     <LoginContext.Provider value={ contextUser }>
