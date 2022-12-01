3df69d94b1729edb616d1064845ca2778fd49d80
Igor Brizack <igor_brizack@hotmail.com>
2022-12-01 12:55:02 -0300

    fix: backend response for login

--- back-end/src/controller/loginCrontoller.js
+++ back-end/src/controller/loginCrontoller.js
@@ -13,7 +13,7 @@ const getToken = async (req, res) => {
     const token = generateToken(data);
     return res.status(200).json({ name, role, email, token });
   } catch (error) {
-    return res.status(400).json({ message: error.message });
+    return res.status(404).json({ message: error.message });
   }
 };
 
