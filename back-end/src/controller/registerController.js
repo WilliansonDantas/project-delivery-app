01464638d521c2890037e5a5ce6ee5db744c44fb
Pedro Costa dos Santos <92334954+PedroC0sta@users.noreply.github.com>
2022-12-01 14:39:03 -0300

    fix(/register): default customer

--- back-end/src/controller/registerController.js
+++ back-end/src/controller/registerController.js
@@ -2,7 +2,7 @@ const md5 = require('md5');
 const registerUser = require('../service/registerService');
 
 const register = async (req, res) => {
-  const { name, email, password } = req.body;
+  const { body: { name, email, password } } = req;
   try {
     await registerUser(name, email, md5(password));
     return res.status(201).json('Created');
--- back-end/src/middlewares/validateRegisterMiddleware.js
+++ back-end/src/middlewares/validateRegisterMiddleware.js
@@ -1,9 +1,9 @@
-const registerSchema = require('../joi/schemas');
+const { registerSchema } = require('../joi/schemas');
 
 const registerValidation = (req, res, next) => {
-  const { body: { email, password } } = req;
+  const { body: { name, email, password } } = req;
 
-    const { error } = registerSchema.validate({ email, password });
+    const { error } = registerSchema.validate({ name, email, password });
     if (error) return res.status(400).json({ message: error.message });
    
     next();
