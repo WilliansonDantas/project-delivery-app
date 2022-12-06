d046b747a84174d3ef61d70ecd00e9307f9713cb
vinicius cardoso <cardosovinicius87@gmail.com>
2022-12-06 16:08:50 -0300

    feat: página de produtos

---
+++ front-end/src/pages/Products.js
@@ -0,0 +1,12 @@
+import Navbar from '../components/Navbar';
+
+function Products() {
+  return (
+    <div>
+      <Navbar> </Navbar>
+      {/* card dos produtos */}
+    </div>
+  );
+}
+
+export default Products;
