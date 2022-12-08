2602a4719c07898042b11fcf705afc0fb3f44e4c
Willianson <williansondantas@gmail.com>
2022-12-08 20:48:04 -0300

    feat: inserção da rota customer/orders

--- front-end/src/pages/OrdersId.js
+++ front-end/src/pages/OrdersId.js
@@ -2,27 +2,79 @@ import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import { getData } from '../services/requests';
 import Navbar from '../components/Navbar';
+import TableOrderDetail from '../components/TableOrderDetail';
 
 function OrdersId() {
   const [productsDetails, setProductsDetails] = useState([]);
   const [total, setTotal] = useState(0);
+  const [arrayData, setArrayData] = useState([]);
+  const [disabled, setDisabled] = useState(true);
+  const [date, setDate] = useState('');
+  const [status, setStatus] = useState('');
   const { id } = useParams();
+
+  const caracteres = 10;
 
   useEffect(() => {
     const orderIdDetail = async () => {
       const data = await getData(`/sale/${id}`);
-      const { totalPrice } = await data;
+      const { totalPrice, products } = await data;
       setTotal(totalPrice);
-      console.log(data);
-      setProductsDetails(data);
+      setProductsDetails(products);
+      setArrayData(data);
+      setDate(data.saleDate);
+      setStatus(data.status);
     };
     orderIdDetail();
   }, []);
+
+  useEffect(() => {
+    const statusVerify = () => {
+      if (status === 'Em Trânsito') {
+        setDisabled(false);
+      }
+    };
+    statusVerify();
+  }, [status]);
+
+  const buttonDisabled = () => {
+    setStatus('Entregue');
+  };
+
+  const dateFix = date.slice(0, caracteres).split('-').reverse().join('/');
 
   return (
     <div>
       <Navbar />
       <h1>Detalhe do Pedido</h1>
+      <p
+        data-testid="customer_order_details__element-order-details-label-order-id"
+      >
+        { `Pedido: ${id}` }
+      </p>
+      <p
+        data-testid="customer_order_details__element-order-details-label-seller-name"
+      >
+        { `${arrayData.sellerName}` }
+      </p>
+      <p
+        data-testid="customer_order_details__element-order-details-label-order-date"
+      >
+        { dateFix }
+      </p>
+      <p
+        data-testid="customer_order_details__element-order-details-label-delivery-status"
+      >
+        { `${status}` }
+      </p>
+      <button
+        type="button"
+        data-testid="customer_order_details__button-delivery-check"
+        disabled={ disabled }
+        onClick={ buttonDisabled }
+      >
+        MARCAR COMO ENTREGUE
+      </button>
       <table>
         <thead>
           <tr>
@@ -36,50 +88,18 @@ function OrdersId() {
         <tbody>
           {productsDetails && (
             productsDetails.map((el, index) => (
-              <tr key={ index }>
-                <td
-                  data-testid={
-                    `customer_order_details__element-order-table-item-number-${index}`
-                  }
-                >
-                  {index + 1}
-                </td>
-                <td
-                  data-testid={
-                    `customer_order_details__element-order-table-name-${index}`
-                  }
-                >
-                  {el.products[index].name}
-                </td>
-                <td
-                  data-testid={
-                    `customer_order_details__element-order-table-quantity-${index}`
-                  }
-                >
-                  {el.products[index].quantity}
-                </td>
-                <td
-                  data-testid={
-                    `customer_order_details__element-order-table-unit-price-${index}`
-                  }
-                >
-                  {`R$ ${(el.products[index].price).toString().replace('.', ',')}`}
-                </td>
-                <td
-                  data-testid={
-                    `customer_order_details__element-order-table-sub-total-${index}`
-                  }
-                >
-                  {`R$ ${((Number(el.products[index].quantity)
-                * Number(el.products[index].price))
-                    .toFixed(2)).toString().replace('.', ',')}`}
-                </td>
-              </tr>
+              <TableOrderDetail
+                key={ index }
+                index={ index }
+                name={ el.name }
+                quantity={ el.quantity }
+                price={ el.price }
+              />
             )))}
         </tbody>
       </table>
       <span data-testid="customer_order_details__element-order-total-price">
-        {`Total ${total}`}
+        {`${((Number(total)).toFixed(2)).toString().replace('.', ',')}`}
       </span>
     </div>
   );
