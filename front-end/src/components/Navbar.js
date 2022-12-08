8f056fed88443d1d6cddc8d2bc888e999e0b0f8d
Willianson <williansondantas@gmail.com>
2022-12-08 15:01:25 -0300

    fix: alteracões básicos na estrutura do código

--- front-end/src/components/Navbar.js
+++ front-end/src/components/Navbar.js
@@ -11,26 +11,28 @@ function Navbar() {
   };
 
   return (
-    <ul>
-      <li data-testid="customer_products__element-navbar-link-products">
-        <a href="/customer/products">Home</a>
-      </li>
-      <li data-testid="customer_products__element-navbar-link-orders">
-        <a href="/customer/orders">Meus Pedidos</a>
-      </li>
-      <li data-testid="customer_products__element-navbar-user-full-name">
-        <a href="/customer/products">{ userLoggedIn.name }</a>
-      </li>
-      <li>
-        <button
-          data-testid="customer_products__element-navbar-link-logout"
-          type="button"
-          onClick={ () => logout() }
-        >
-          Sair
-        </button>
-      </li>
-    </ul>
+    <nav>
+      <ul>
+        <li data-testid="customer_products__element-navbar-link-products">
+          <a href="/customer/products">Home</a>
+        </li>
+        <li data-testid="customer_products__element-navbar-link-orders">
+          <a href="/customer/orders">Meus Pedidos</a>
+        </li>
+        <li data-testid="customer_products__element-navbar-user-full-name">
+          <a href="/customer/products">{ userLoggedIn.name }</a>
+        </li>
+        <li>
+          <button
+            data-testid="customer_products__element-navbar-link-logout"
+            type="button"
+            onClick={ () => logout() }
+          >
+            Sair
+          </button>
+        </li>
+      </ul>
+    </nav>
   );
 }
 
