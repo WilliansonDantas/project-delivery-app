c1eb5e24bd96be3e2d00d6dde94f0f1192de6528
Willianson <williansondantas@gmail.com>
2022-12-08 21:30:26 -0300

    feat: insere estrutura da página Orders

--- front-end/src/pages/Orders.js
+++ front-end/src/pages/Orders.js
@@ -1,10 +1,33 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { v4 as uuidv4 } from 'uuid';
+import CardOrder from '../components/CardOrder';
 import Navbar from '../components/Navbar';
 
 function Orders() {
+  const [orders, setOrders] = useState([]);
+
+  useEffect(() => {
+    const ordersPlaced = async () => {
+      const data = await getData('/sales');
+      setOrders(data);
+    };
+    ordersPlaced();
+  }, []);
+
   return (
     <div>
       <Navbar />
+      {orders.length > 1 && (
+        orders.map((order) => (
+          <CardOrder
+            key={ uuidv4() }
+            id={ order.id }
+            order={ order.order }
+            status={ order.status }
+            date={ order.date }
+            price={ order.price }
+          />
+        )))}
     </div>
   );
 }
