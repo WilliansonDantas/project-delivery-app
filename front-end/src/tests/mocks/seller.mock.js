93d65a8701fe8308423b3e2d1a47636d5e2d2f30
Igor Brizack <igor_brizack@hotmail.com>
2022-12-13 13:47:45 -0300

    feat: testes para tela de checkout criado

---
+++ front-end/src/tests/mocks/seller.mock.js
@@ -0,0 +1,11 @@
+const seller = [
+  {
+    id: 2,
+    email: 'fulana@deliveryapp.com',
+    name: 'Fulana Pereira',
+    password: '3c28d2b0881bf46457a853e0b07531c6',
+    role: 'seller',
+  },
+];
+
+module.exports = { seller };
