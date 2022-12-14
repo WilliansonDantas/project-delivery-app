911d0264c16348e93801277fd2d518a767893f3e
Willianson <williansondantas@gmail.com>
2022-12-14 18:37:19 -0300

    'feat: insere rota seller/orders'

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -4,13 +4,13 @@ import Login from './pages/Login';
 import LoginProvider from './contexts/LoginProvider';
 import UserProvider from './contexts/UserProvider';
 import RegisterProvider from './contexts/RegisterProvider';
-
 import Register from './pages/Register';
 import Products from './pages/Products';
 import Checkout from './pages/Checkout';
 import Orders from './pages/Orders';
 import Admin from './pages/Admin';
 import OrdersId from './pages/OrdersId';
+import SellerOrders from './pages/SellerOrders';
 
 function App() {
   return (
@@ -26,7 +26,7 @@ function App() {
           <Route exact path="/customer/products" component={ Products } />
           <Route exact path="/customer/orders/:id" component={ OrdersId } />
           <Route exact path="/customer/orders" component={ Orders } />
-          <Route exact path="/seller/orders" component={ Orders } />
+          <Route exact path="/seller/orders" component={ SellerOrders } />
         </LoginProvider>
         <RegisterProvider>
           <Route path="/admin/manage" component={ Admin } />
