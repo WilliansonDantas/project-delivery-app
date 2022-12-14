34326dee60d86841dcec9f35cbecd55669057816
Igor Brizack <igor_brizack@hotmail.com>
2022-12-14 15:26:47 -0300

    feat: sellers orders route by id, created

--- back-end/src/routes/sellers.js
+++ back-end/src/routes/sellers.js
@@ -1,9 +1,11 @@
 const { Router } = require('express');
-const { getSellers } = require('../controller/sellersController');
+const { getSellers, getSellerOrders } = require('../controller/sellersController');
 const { validateJWT } = require('../middlewares');
 
 const sellerRouter = Router();
 
 sellerRouter.get('/sellers', validateJWT, getSellers);
+
+sellerRouter.get('/sellers/:id/orders', validateJWT, getSellerOrders);
 
 module.exports = sellerRouter;
