547aa0da463b4b9f081328d24dba0baf4999a814
Willianson <williansondantas@gmail.com>
2022-12-15 19:46:44 -0300

    'feat: cria rotas de delete e get para user'

--- back-end/src/service/registerAdmService.js
+++ back-end/src/service/registerAdmService.js
@@ -1,8 +1,13 @@
+const { Op } = require('sequelize');
 const { User } = require('../database/models');
 
 const registerAdmUser = async (name, email, password, role) => {
-  const checkedEmail = await User.findOne({ where: { email } });
-  const checkedName = await User.findOne({ where: { name } });
+  // const checkedEmail = await User.findOne({ where: { email } });
+  const checkedName = await User.findOne({ where: { 
+    [
+      Op.or
+    ]: [{email}, {name}]
+   } });
   if (checkedEmail || checkedName) {
     throw new Error('Conflict');
   }
