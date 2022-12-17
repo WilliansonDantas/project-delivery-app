d3d1d32cdead4921d62fb36d6e670590fcc66073
Willianson <williansondantas@gmail.com>
2022-12-17 14:52:17 -0300

    'fix: torna o card um button'

--- front-end/src/components/CardOrder.js
+++ front-end/src/components/CardOrder.js
@@ -18,30 +18,34 @@ function CardOrder({ id, order, status, date, price }) {
 
   const history = useHistory();
   return (
-    <div>
-      <button
-        onClick={ () => history.push(`/customer/orders/${id}`) }
-        data-testid={ `customer_orders__element-order-id-${id}` }
-        type="button"
-      >
-        { order }
-      </button>
-      <p
-        data-testid={ `customer_orders__element-delivery-status-${id}` }
-      >
-        { status }
-      </p>
-      <p
-        data-testid={ `customer_orders__element-order-date-${id}` }
-      >
-        { dataAtualFormatada() }
-      </p>
-      <p
-        data-testid={ `customer_orders__element-card-price-${id}` }
-      >
-        { price.replace('.', ',') }
-      </p>
-    </div>
+    <button
+      onClick={ () => history.push(`/customer/orders/${id}`) }
+      type="button"
+    >
+      <div>
+        <p
+          data-testid={ `customer_orders__element-order-id-${id}` }
+        >
+
+          { order }
+        </p>
+        <p
+          data-testid={ `customer_orders__element-delivery-status-${id}` }
+        >
+          { status }
+        </p>
+        <p
+          data-testid={ `customer_orders__element-order-date-${id}` }
+        >
+          { dataAtualFormatada() }
+        </p>
+        <p
+          data-testid={ `customer_orders__element-card-price-${id}` }
+        >
+          { price.replace('.', ',') }
+        </p>
+      </div>
+    </button>
   );
 }
 
